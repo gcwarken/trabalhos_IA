7e24c1a0926a1aa9c322de0b682126b428c64f59
gcwarken <gcwarken@gmail.com>
2025-04-16 23:30:12 -0300

    ajuste t1.py

--- t1/t1.py
+++ t1/t1.py
@@ -4,74 +4,54 @@ import matplotlib.animation as animation
 
 import alegrete
 
-def carregaDataset():
-    data = np.genfromtxt('alegrete.csv',delimiter = ',')
-    data=np.array(data)
-    return data
+dataset = np.genfromtxt('alegrete.csv',delimiter = ',')
+dataset=np.array(dataset)
 
-def plotDados(titulo, dataset):
-    plt.figure(figsize=(6, 2))
-    plt.scatter(dataset[:,0], dataset[:,1])
-    plt.xlabel('x')
-    plt.title(titulo)
-    plt.ylabel('y')
-    plt.savefig('plotDados.png')
+#Gráfico dos dados
+plt.figure(figsize=(6, 2))
+plt.scatter(dataset[:,0], dataset[:,1])
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('dataset Alegrete.csv')
+plt.savefig('plotDados.png')
 
-def eqm(dataset):
-    mse_list = [alegrete.compute_mse(b, w, dataset) for b, w in zip(b_history, w_history)]
-    print(f"EQM final: {mse_list[-1]}")
-    plt.plot(mse_list)
-    plt.xlabel('Epoca/iteracao')
-    plt.ylabel('EQM')
-    plt.savefig('eqm.png')
+"""
+### Execucao da regressao linear e calculo do EQM por epoca/iteracao
 
-def curvaFinal(dataset):
-    #Imprimir parâmetros otimizados
-    print (f'Curva encontrada: {final_w}*x + {final_b}')
-
-    #plota os dados
-    plt.figure(figsize=(4, 2))
-    plt.scatter(dataset[:,0], dataset[:,1])
-plot
-    # plota a curva de ajuste
-    pred = final_w*dataset[:,0] + final_b
-    plt.plot(dataset[:,0], pred, c='r')
-    plt.savefig('curvaFinal.png')
-
-def descida():
-    fig = plt.figure(figsize=(4, 2))
-    ax = fig.add_subplot(111)
-
-    # conjunto de dados
-    ax.scatter(dataset[:,0], dataset[:,1])
-
-    # linha com os valores iniciais dos parametros
-    pred = w_history[0]*dataset[:,0] + b_history[0]
-    line, = ax.plot(dataset[:,0], pred, '-',c='r')
-
-    # funcao que atualiza a linha a cada passo
-#    def animate(i):
-#        pred = w_history[i] * dataset[:,0] + b_history[i]
-#        line.set_ydata(pred)
-#        return line,
-#
-#    # mude interval para trocar a velocidade da animacao
-#    ani = animation.FuncAnimation(fig, animate, frames=len(b_history), interval=20, save_count=50)
-#    HTML(ani.to_jshtml())
-
-
-####################################################################################################
-
-dataset = carregaDataset()
-plotDados("Base alegrete.csv", dataset)
+Considere o parâmetro b o coeficiente linear (theta_0, visto em aula) e w o coeficiente angular (theta_1, visto em aula).
+"""
 
 b_history, w_history = alegrete.fit(
-    dataset, b=0, w=0,
-    alpha=0.1, num_iterations=100
+    dataset,
+    b=1,
+    w=0.7,
+    alpha=0.01,
+    num_iterations=1000
 )
 
 # valores finais de theta0 e theta1
 final_b, final_w = b_history[-1], w_history[-1]
 
-eqm()
-curvaFinal()
+mse_list = [alegrete.compute_mse(b, w, dataset) for b, w in zip(b_history, w_history)]
+print(f"EQM final: {mse_list[-1]}")
+
+plt.cla()
+plt.plot(mse_list)
+plt.xlabel('Epoca/iteracao')
+plt.ylabel('EQM')
+plt.savefig('eqm.png')
+
+"""
+### Plot da curva final"""
+
+#Imprimir parâmetros otimizados
+print (f'Curva encontrada: {final_w}*x + {final_b}')
+
+#plota os dados
+plt.figure(figsize=(4, 2))
+plt.scatter(dataset[:,0], dataset[:,1])
+
+# plota a curva de ajuste
+pred = final_w*dataset[:,0] + final_b
+plt.plot(dataset[:,0], pred, c='r')
+plt.savefig('curvaFinal.png')
