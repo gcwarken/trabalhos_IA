34261ec02e4f6360c347b651161626c60862dcab
DBRivoire <diogobrivoire21@gmail.com>
2025-06-19 23:48:19 -0300

    Update othello_minimax_mask.py

--- t4/advsearch/your_agent/othello_minimax_mask.py
+++ t4/advsearch/your_agent/othello_minimax_mask.py
@@ -12,7 +12,7 @@ from .minimax import minimax_move
 
 # mask template adjusted from https://web.fe.up.pt/~eol/IA/MIA0203/trabalhos/Damas_Othelo/Docs/Eval.html
 # could optimize for symmetries but just put all values here for coding speed :P
-# DO NOT CHANGE!
+# DO NOT CHANGE! 
 EVAL_TEMPLATE = [
     [100, -30, 6, 2, 2, 6, -30, 100],
     [-30, -50, 1, 1, 1, 1, -50, -30],
@@ -33,36 +33,32 @@ def make_move(state) -> Tuple[int, int]:
     """
 
     # o codigo abaixo apenas retorna um movimento aleatorio valido para
-    # a primeira jogada
+    # a primeira jogada 
     # Remova-o e coloque uma chamada para o minimax_move (que vc implementara' no modulo minimax).
     # A chamada a minimax_move deve receber sua funcao evaluate como parametro.
 
-    return random.choice([(2, 3), (4, 5), (5, 4), (3, 2)])
+    return minimax_move(state, 5, evaluate_mask)
 
 
 def evaluate_mask(state, player:str) -> float:
     """
-    Evaluates an othello state from the point of view of the given player.
-    If the state is terminal, returns its utility.
+    Evaluates an othello state from the point of view of the given player. 
+    If the state is terminal, returns its utility. 
     If non-terminal, returns an estimate of its value based on the positional value of the pieces.
     You must use the EVAL_TEMPLATE above to compute the positional value of the pieces.
     :param state: state to evaluate (instance of GameState)
     :param player: player to evaluate the state for (B or W)
     """
     board = state.get_board().tiles
-    potuacao_pretas = 0
-    pontuacao_brancas = 0
-
-    for x in range(8):
-        for y in range(8):
-            if board[x][y] == "B":
-                potuacao_pretas += EVAL_TEMPLATE[x][y]
-            elif board[x][y] == "W":
-                pontuacao_brancas += EVAL_TEMPLATE[x][y]
-
-    diferenca_pontos = potuacao_pretas - pontuacao_brancas
-
-    if player == "B":
-        return diferenca_pontos
-    else:
-       return -diferenca_pontos
+    b_score = 0
+    w_score = 0
+    
+    for row in range(8):
+        for col in range(8):
+            if board[row][col] == "B":
+                b_score += EVAL_TEMPLATE[row][col]
+            elif board[row][col] == "W":
+                w_score += EVAL_TEMPLATE[row][col]
+    difference = b_score - w_score
+    
+    return difference if player == "B" else -difference
